18e3f485232afcdb7612e3252758330ddbc44557
yunusyiilmaz <yns.ylmz1381@gmail.com>
2022-08-12 18:21:36 +0300

    Kare piramidal
    Kare piramidal

---
+++ KARE PİRAMİDAL SAYILAR/KARE PİRAMİDAL SAYILAR/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KARE_PİRAMİDAL_SAYILAR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Yöntem
+            //int toplam = 0;
+            //Console.Write("BİR SAYI GİRİNİZ :");
+            //int sayi=Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= sayi; i++)
+            //{
+            //    toplam = 0;
+            //    for(int j = 1;j<=i; j++)
+            //    {
+            //        toplam += j * j;
+            //        Console.Write(j+"^2");
+            //        if(j!=i)
+            //        {
+            //            Console.Write("+");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("=");
+            //        }
+            //    }
+            //    Console.WriteLine(toplam);
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+
+            //2.Yöntem
+            int toplam = 0;
+            Console.Write("BİR SAYI GİRİNİZ :");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            string line = string.Empty;
+            for (int i = 1; i <= sayi; i++)
+            {
+                toplam += i * i;
+                if (line == "")
+                    line += i.ToString() + "^2";
+                else
+                    line += "+" + i.ToString() + "^2";
+                Console.Write(line + "=");
+                Console.WriteLine(toplam);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ KARE PİRAMİDAL SAYILAR/KARE PİRAMİDAL SAYILAR/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("KARE PİRAMİDAL SAYILAR")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KARE PİRAMİDAL SAYILAR")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("eb77e479-529b-4401-8b68-3ff47496652f")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
